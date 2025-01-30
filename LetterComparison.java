d48afc3e2e479cc706bc41181068957c457e3d0d
Bhuvanesh K. S <bhuvansanks@gmail.com>
2025-01-30 21:39:29 +0530

    Add files via upload

---
+++ LetterComparison.java
@@ -0,0 +1,16 @@
+class LetterComparison {
+    public static void main(String[] args) {
+        
+        char letterA = 'A';
+        char letterB = 'B';
+        
+    
+        System.out.println("Comparison between 'A' and 'B':");
+        System.out.println("'A' < 'B' : " + (letterA < letterB)); 
+        System.out.println("'A' <= 'B' : " + (letterA <= letterB));  
+        System.out.println("'A' > 'B' : " + (letterA > letterB)); 
+        System.out.println("'A' >= 'B' : " + (letterA >= letterB));  
+        System.out.println("'A' == 'B' : " + (letterA == letterB));  
+        System.out.println("'A' != 'B' : " + (letterA != letterB));  
+    }
+}
